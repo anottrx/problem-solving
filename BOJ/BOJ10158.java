36bd8cae62dc88ea01daed49d6d41ee6c16b9313
ryuna <59449215+anottrx@users.noreply.github.com>
2021-09-08 23:22:48 +0900

    Revised : 백준저지 10158번 개미

--- BOJ/BOJ10158.java
+++ BOJ/BOJ10158.java
@@ -1,5 +1,5 @@
 // 출처: 백준저지 10158번 개미 https://www.acmicpc.net/problem/10158 
-// 시간 초과
+// 자바8은 통과. 자바11은 시간 초과
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -8,17 +8,17 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class BOJ10158 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int w = Integer.parseInt(st.nextToken()); // w와 h는 자연수. (2 ≤ w,h ≤ 40,000)
+        int w = Integer.parseInt(st.nextToken());
         int h = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int x = Integer.parseInt(st.nextToken()); // 개미의 초기 위치 p와 q도 자연수. (0 < p < w)
-        int y = Integer.parseInt(st.nextToken()); // (0 < q < h)
-        int t = Integer.parseInt(br.readLine()); // 계산할 시간 (1 ≤ t ≤ 200,000,000)
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(br.readLine());
 
         x = (x + t) % (w * 2);
         y = (y + t) % (h * 2);
