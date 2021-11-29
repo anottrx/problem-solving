edebbbfe054f6c969064dc5c992d88f8f175b1ad
ryuna <59449215+anottrx@users.noreply.github.com>
2021-11-29 23:36:23 +0900

    백준저지 11403번 경로 찾기

---
+++ BOJ/[11403] 경로 찾기/Main.java
@@ -0,0 +1,40 @@
+// 백준저지 11403번 경로 찾기 https://www.acmicpc.net/problem/11403
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] dist = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                dist[i][j] = sc.nextInt();
+                if (dist[i][j] == 0) {
+                    dist[i][j] = 10001;
+                }
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dist[i][j] == 10001) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
