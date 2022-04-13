2dededbd2836a14a0b4c2ecf247727ac47a90843
ryuna <anottrx@gmail.com>
2022-04-13 23:31:50 +0900

    Solve : 프로그래머스 같은 숫자는 싫어

---
+++ PROGRAMMERS/같은숫자는싫어.js
@@ -0,0 +1,12 @@
+// 프로그래머스 같은 숫자는 싫어 https://programmers.co.kr/learn/courses/30/lessons/12906
+
+function solution(arr) {
+  const answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] !== arr[i]) {
+      answer.push(arr[i]);
+    }
+  }
+  return answer;
+}
