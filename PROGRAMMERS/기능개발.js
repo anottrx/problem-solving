ce648e443f8c9dab989cedf868d496a253049f96
ryuna <anottrx@gmail.com>
2022-05-13 23:00:18 +0900

    Comment : 프로그래머스 기능개발 풀이 추가

--- PROGRAMMERS/기능개발.js
+++ PROGRAMMERS/기능개발.js
@@ -5,18 +5,18 @@ function solution(progresses, speeds) {
 
   const stack = [];
   for (let i = 0; i < progresses.length; i++) {
-    let day = Math.ceil((100 - progresses[i]) / speeds[i]);
+    let day = Math.ceil((100 - progresses[i]) / speeds[i]); // 필요한 날짜 계산
     if (stack.length > 0 && stack[0] >= day) {
-      stack.push(day);
     } else {
+      // 만약 스택에서 가장 오래된 일보다 처리가 오래 걸린다면 스택의 일 개수를 answer에 넣고 스택 비우기
       answer.push(stack.length);
       stack.length = 0;
-      stack.push(day);
     }
+    stack.push(day);
   }
 
-  answer.push(stack.length);
-  answer.splice(0, 1);
+  answer.push(stack.length); // 다 끝나고 스택에 남은 일 개수 answer에 넣기
+  answer.splice(0, 1); // 처음에 들어간 0 제거하기
 
   return answer;
 }
