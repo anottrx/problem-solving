6ac9b9243e35fae14e9eb26664a0a2c562e35dc3
ryuna <anottrx@gmail.com>
2022-08-16 19:16:24 +0900

    Refactor : 프로그래머스 같은숫자는싫어

--- PROGRAMMERS/같은숫자는싫어.js
+++ PROGRAMMERS/같은숫자는싫어.js
@@ -1,6 +1,11 @@
 // 프로그래머스 같은 숫자는 싫어 https://programmers.co.kr/learn/courses/30/lessons/12906
-
 function solution(arr) {
+  const answer = arr.filter((el, index) => index > 0 && arr[index - 1] !== el);
+  answer.splice(0, 0, arr[0]);
+  return answer;
+}
+
+function befSolution(arr) {
   const answer = [];
   answer.push(arr[0]);
   for (let i = 1; i < arr.length; i++) {
