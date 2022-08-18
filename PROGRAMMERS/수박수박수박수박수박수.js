dad53534c533ac4677701dedaf1a3ba64572e2f4
ryuna <anottrx@gmail.com>
2022-08-18 17:04:11 +0900

    Refactor : 프로그래머스 수박수박수박수박수박수?

--- PROGRAMMERS/수박수박수박수박수박수.js
+++ PROGRAMMERS/수박수박수박수박수박수.js
@@ -1,6 +1,10 @@
 // 프로그래머스 수박수박수박수박수박수? https://programmers.co.kr/learn/courses/30/lessons/12922
 
 function solution(n) {
+  return n % 2 === 0 ? "수박".repeat(n / 2) : "수박".repeat((n - 1) / 2) + "수";
+}
+
+function befSolution(n) {
   var answer = "";
   const nn = Math.floor(n / 2);
   for (let i = 0; i < nn; i++) {
