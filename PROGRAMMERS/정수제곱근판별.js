3993e36f59da25a7971030b9b919df5e4d048c77
ryuna <anottrx@gmail.com>
2022-09-11 22:31:01 +0900

    Refactor : 프로그래머스 정수 제곱근 판별

--- PROGRAMMERS/정수제곱근판별.js
+++ PROGRAMMERS/정수제곱근판별.js
@@ -3,10 +3,9 @@
 function solution(n) {
   let answer = -1;
   for (let i = 1; i <= Math.ceil(n / 2); i++) {
-    // 1부터 시작하기
     if (n == i * i) {
-      answer = (i + 1) * (i + 1);
-      break; // 제곱이 될 수 있는 경우는 1개만 있기 떄문에 찾으면 끝내기
+      answer = Math.pow(i + 1, 2);
+      break;
     }
   }
   return answer;
