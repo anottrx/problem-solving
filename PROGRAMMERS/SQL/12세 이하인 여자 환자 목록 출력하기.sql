a3b17f6e0cd099a250f6bad5b45bfac76e068de0
ryuna <anottrx@gmail.com>
2022-10-28 17:22:24 +0900

    Solve : 프로그래머스 12세 이하인 여자 환자 목록 출력하기

---
+++ PROGRAMMERS/SQL/12세 이하인 여자 환자 목록 출력하기.sql
@@ -0,0 +1,8 @@
+-- 프로그래머스 12세 이하인 여자 환자 목록 출력하기 https://school.programmers.co.kr/learn/courses/30/lessons/132201
+
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE') AS TLNO
+    FROM PATIENT
+    WHERE AGE <= 12
+    AND GEND_CD = 'W'
+    ORDER BY AGE DESC, PT_NAME;
+    
