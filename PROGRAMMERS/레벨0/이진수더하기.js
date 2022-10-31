89986e71d8d260445474383153ce25bb1003a2ea
ryuna <anottrx@gmail.com>
2022-10-31 10:13:38 +0900

    solve: 프로그래머스 이진수 더하기 (#1)

---
+++ PROGRAMMERS/레벨0/이진수더하기.js
@@ -0,0 +1,5 @@
+// 프로그래머스 이진수 더하기 https://school.programmers.co.kr/learn/courses/30/lessons/120885
+
+function solution(bin1, bin2) {
+  return (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
+}
