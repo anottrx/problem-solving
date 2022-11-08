147c8651ac3f29d588e8ac2ad4e6738395f01494
ryuna <anottrx@gmail.com>
2022-11-08 23:47:09 +0900

    refactor: 프로그래머스 3진법 뒤집기

--- PROGRAMMERS/레벨1/3진법뒤집기.js
+++ PROGRAMMERS/레벨1/3진법뒤집기.js
@@ -1,19 +1,5 @@
 // 프로그래머스 3진법 뒤집기 https://programmers.co.kr/learn/courses/30/lessons/68935
 
 function solution(n) {
-  let answer = 0;
-
-  let tempStr = "";
-  while (n > 0) {
-    tempStr = String(n % 3) + tempStr;
-    n = Math.floor(n / 3);
-  }
-  tempStr = tempStr.split("").reverse().join("");
-
-  let nth = tempStr.length - 1;
-  tempStr.split("").forEach((x) => {
-    answer = answer + x * Math.pow(3, nth--);
-  });
-
-  return answer;
+  return parseInt(n.toString(3).split("").reverse().join(""), 3);
 }
