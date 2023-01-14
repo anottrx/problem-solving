7336e6f6cba7b0043fa9b2d6d2ace2e5d8be3ae8
ryuna <anottrx@gmail.com>
2023-01-14 23:02:59 +0900

    style: 프로그래머스 가장 가까운 같은 글자

--- 프로그래머스/레벨1/가장가까운같은글자.js
+++ 프로그래머스/레벨1/가장가까운같은글자.js
@@ -1,17 +1,17 @@
 // 프로그래머스 가장 가까운 같은 글자 https://school.programmers.co.kr/learn/courses/30/lessons/142086
 
 function solution(s) {
-    const map = new Map();
-    return s.split('').map((v, i) => {
-        if(!map.has(v)) {
-            map.set(v, [i]);
-            return -1;
-        } else {
-            const indexs = map.get(v);
-            let res = i - indexs[indexs.length - 1];
-            indexs.push(i);
-            map.set(v, indexs);
-            return res;
-        }
-    })
+  const map = new Map();
+  return s.split("").map((v, i) => {
+    if (!map.has(v)) {
+      map.set(v, [i]);
+      return -1;
+    } else {
+      const indexs = map.get(v);
+      let res = i - indexs[indexs.length - 1];
+      indexs.push(i);
+      map.set(v, indexs);
+      return res;
+    }
+  });
 }
