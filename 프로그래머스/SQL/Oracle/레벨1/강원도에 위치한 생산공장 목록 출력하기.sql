af12bbefdbdcf5b7a6223364b5d2ed4fad537a02
ryuna <anottrx@gmail.com>
2023-03-27 22:31:40 +0900

    solve: 프로그래머스 강원도에 위치한 생산공장 목록 출력하기 (#6)

---
+++ 프로그래머스/SQL/Oracle/레벨1/강원도에 위치한 생산공장 목록 출력하기.sql
@@ -0,0 +1,6 @@
+-- 프로그래머스 강원도에 위치한 생산공장 목록 출력하기 https://school.programmers.co.kr/learn/courses/30/lessons/131112?language=oracle
+
+SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
+FROM FOOD_FACTORY
+WHERE ADDRESS LIKE '강원도%'
+ORDER BY FACTORY_ID;
