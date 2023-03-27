2dde4502497167dc3314d5706958debe9813b7a1
ryuna <anottrx@gmail.com>
2023-03-27 22:12:28 +0900

    solve: 프로그래머스 아픈 동물 찾기 (#6)

---
+++ 프로그래머스/SQL/Oracle/레벨1/아픈 동물 찾기.sql
@@ -0,0 +1,6 @@
+-- 프로그래머스 아픈 동물 찾기 https://school.programmers.co.kr/learn/courses/30/lessons/59036?language=oracle
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID;
