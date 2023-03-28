2c3697264abb6ad55490570e12a3c7e6946ac9d5
ryuna <anottrx@gmail.com>
2023-03-28 23:59:39 +0900

    solve: 프로그래머스 경기도에 위치한 식품창고 목록 출력하기 (#6)

---
+++ 프로그래머스/SQL/Oracle/레벨1/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -0,0 +1,9 @@
+-- 프로그래머스 경기도에 위치한 식품창고 목록 출력하기 https://school.programmers.co.kr/learn/courses/30/lessons/131114
+
+SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS,
+CASE WHEN FREEZER_YN IS NULL THEN 'N'
+    ELSE FREEZER_YN
+END AS FREEZER_YN
+FROM FOOD_WAREHOUSE
+WHERE ADDRESS LIKE '경기도%'
+ORDER BY WAREHOUSE_ID;
