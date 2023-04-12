3bb7c7b7363225e33ddbc97e8ee138acb33fbdda
ryuna <anottrx@gmail.com>
2023-04-12 23:51:07 +0900

    solve: 프로그래머스 과일로 만든 아이스크림 고르기 (#6)

---
+++ 프로그래머스/SQL/Oracle/레벨1/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,10 @@
+-- 프로그래머스 과일로 만든 아이스크림 고르기 https://school.programmers.co.kr/learn/courses/30/lessons/133025?language=oracle
+
+SELECT a.FLAVOR 
+FROM FIRST_HALF a
+JOIN ICECREAM_INFO b
+ON a.FLAVOR = b.FLAVOR
+WHERE TOTAL_ORDER > 3000
+    AND INGREDIENT_TYPE = 'fruit_based'
+ORDER BY TOTAL_ORDER DESC;
+
