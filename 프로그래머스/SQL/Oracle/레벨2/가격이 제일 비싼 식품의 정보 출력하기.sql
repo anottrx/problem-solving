43421153d62fe7ae41e2f93904236d51064e8f8f
ryuna <anottrx@gmail.com>
2023-08-06 01:11:30 +0900

    refactor: 프로그래머스 가격이 제일 비싼 식품의 정보 출력하기 (#7)

--- 프로그래머스/SQL/Oracle/레벨2/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ 프로그래머스/SQL/Oracle/레벨2/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -1,3 +1,5 @@
+--프로그래머스 가격이 제일 비싼 식품의 정보 출력하기 https://school.programmers.co.kr/learn/courses/30/lessons/131115
+
 SELECT PRODUCT_ID,PRODUCT_NAME,PRODUCT_CD,CATEGORY,PRICE
 FROM FOOD_PRODUCT
 WHERE PRICE = 
