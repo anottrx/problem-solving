7d8802d9efcfa33ccee6f4430f7cd1c89c10fabc
ryuna <anottrx@gmail.com>
2023-08-13 00:03:44 +0900

    solve: 프로그래머스 재구매가 일어난 상품과 회원 리스트 구하기 (#7)

---
+++ 프로그래머스/Oracle/레벨2/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,8 @@
+--프로그래머스 재구매가 일어난 상품과 회원 리스트 구하기 https://school.programmers.co.kr/learn/courses/30/lessons/131536
+
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) >= 2 
+ORDER BY USER_ID, PRODUCT_ID DESC
+;
